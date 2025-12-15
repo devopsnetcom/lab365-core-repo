32874e08306b39e48080d53c86bc8b0bb9b585d5
Saileswar Saha <68107017+SaileswarSaha@users.noreply.github.com>
2025-12-15 22:45:38 +0530

    removed the existi g check

--- terraform-modules/role_definition/main.tf
+++ terraform-modules/role_definition/main.tf
@@ -1,31 +1,19 @@
 
-# Retrieve existing parent role definition if it exists
-data "azurerm_role_definition" "existing_parent" {
-  name  = var.parentRole.name
-  scope = replace(var.parentRole.assignableScopes[0],"{subscriptionid}",var.subscription_id)
-}
-
-# Generate a UUID for the role definition if it does not already exist
-resource "random_uuid" "parent_role_guid" {
-    count = try(data.azurerm_role_definition.existing_parent.id, null) == null ? 1 : 0
-}
+resource "random_uuid" "parent_role_guid" {}
 
 # Create custom role definition for parent role
 resource "azurerm_role_definition" "parent" {
-    count = try(data.azurerm_role_definition.existing_parent.id, null) == null ? 1 : 0
+  name               = var.parentRole.name
+  role_definition_id = random_uuid.parent_role_guid.result
 
-    name               = var.parentRole.name
-    role_definition_id = random_uuid.parent_role_guid[0].result
+  scope = replace(var.parentRole.assignableScopes[0],"{subscriptionid}",var.subscription_id)
 
-    scope = replace(var.parentRole.assignableScopes[0],"{subscriptionid}",var.subscription_id)
+  description = var.parentRole.description
 
-    description = var.parentRole.description
-
-    permissions {
-        actions          = var.parentRole.permissions[0].actions
-        not_actions      = var.parentRole.permissions[0].notActions
-        data_actions     = var.parentRole.permissions[0].dataActions
-        not_data_actions = var.parentRole.permissions[0].notDataActions
-    }
-    
+  permissions {
+    actions          = var.parentRole.permissions[0].actions
+    not_actions      = var.parentRole.permissions[0].notActions
+    data_actions     = var.parentRole.permissions[0].dataActions
+    not_data_actions = var.parentRole.permissions[0].notDataActions
+  }
 }
--- terraform-modules/role_definition/outputs.tf
+++ terraform-modules/role_definition/outputs.tf
@@ -1,6 +1,3 @@
-output "parent_role_id" {
-  value = try(
-    data.azurerm_role_definition.existing_parent.id,
-    azurerm_role_definition.parent[0].id
-  )
+output "role_name" {
+  value = azurerm_role_definition.parent.name
 }
