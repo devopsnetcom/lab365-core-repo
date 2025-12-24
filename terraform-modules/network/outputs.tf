f6b4a4f85ee313d39d3d4b19e070f69c6305157f
Saileswar Saha <68107017+SaileswarSaha@users.noreply.github.com>
2025-12-24 21:58:01 +0530

    vnet outputs

--- terraform-modules/network/outputs.tf
+++ terraform-modules/network/outputs.tf
@@ -1,16 +1,11 @@
 output "vnet_Name" {
-  value = azurerm_virtual_network.student_vnet.name
+  value = azurerm_virtual_network.shared_vnet.name
 }
 
 output "vnet_Id" {
-  value = azurerm_virtual_network.student_vnet.id
+  value = azurerm_virtual_network.shared_vnet.id
 }
 
 output "subnet_Id" {
   value = azurerm_subnet.student_subnet.*.id
-}
-
-# ✅ Added NSG ID output
-output "nsg_id" {
-  value = azurerm_network_security_group.vm_nsg.id
 }
