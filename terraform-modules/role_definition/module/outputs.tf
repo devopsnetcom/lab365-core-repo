8a38351ca64c39e00b18a6603d783340274754dd
Saileswar Saha <68107017+SaileswarSaha@users.noreply.github.com>
2025-12-26 22:06:15 +0530

    outputs corrected

--- terraform-modules/role_definition/module/outputs.tf
+++ terraform-modules/role_definition/module/outputs.tf
@@ -1,11 +1,11 @@
 output "module_role_name" {
-  value = azurerm_role_definition.module.name
+  value = local.module_role_exists ? data.azurerm_role_definition.module_existing.name : azurerm_role_definition.module[0].name
 }
 
 output "module_role_id" {
-  value = azurerm_role_definition.module.id
+  value = local.module_role_exists ? data.azurerm_role_definition.module_existing.id : azurerm_role_definition.module[0].id
 }
 
 output "module_role_definition_id" {
-  value = azurerm_role_definition.module.role_definition_id
+  value = local.module_role_exists ? data.azurerm_role_definition.module_existing.id : azurerm_role_definition.module[0].role_definition_id
 }
