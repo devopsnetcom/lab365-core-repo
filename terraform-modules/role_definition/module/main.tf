ba7f25640326983ce9e0832c5f5255995310e627
Saileswar Saha <68107017+SaileswarSaha@users.noreply.github.com>
2025-12-26 21:51:49 +0530

    syntax corrected

--- terraform-modules/role_definition/module/main.tf
+++ terraform-modules/role_definition/module/main.tf
@@ -34,19 +34,16 @@ resource "random_uuid" "module_role_guid" {
   count = local.module_role_exists ? 0 : 1
 }
 
-locals {
-  resolved_module_role_definition_id = local.module_role_exists ? data.azurerm_role_definition.module_existing.id : azurerm_role_definition.module[0].id
-}
-
 # Create custom role definition for module role
 resource "azurerm_role_definition" "module" {
+  count = local.module_role_exists ? 0 : 1
   
   name               = replace(
                         replace(var.moduleRole.nameTemplate, "{course}", var.course_name),
                         "{module}", var.module_name
                       )
 
-  role_definition_id = local.resolved_module_role_definition_id
+  role_definition_id = random_uuid.module_role_guid[0].result
 
   scope = local.resolved_scope
 
