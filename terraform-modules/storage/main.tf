e795c9aeed3f1f1f02cafd31f45c45c172325538
Saileswar Saha <68107017+SaileswarSaha@users.noreply.github.com>
2025-12-30 22:14:38 +0530

    refefrence corrected

--- terraform-modules/storage/main.tf
+++ terraform-modules/storage/main.tf
@@ -1,22 +1,23 @@
 
 # Terraform module to create an Azure Storage Account and a Storage Queue
 resource "azurerm_storage_account" "sa" {
-  name                      = var.storage_account.name
-  resource_group_name       = var.storage_account.resource_group
-  location                  = var.storage_account.location
+  name                      = var.storageAccount.name
+  resource_group_name       = var.storageAccount.resource_group
+  location                  = var.storageAccount.location
 
-  access_tier               = var.storage_account.access_tier
-  account_kind              = var.storage_account.kind
-  account_replication_type  = var.storage_account.account_replication_type
-  account_tier              = var.storage_account.account_tier
-  min_tls_version          = var.storage_account.tls_version
+  access_tier               = var.storageAccount.access_tier
+  account_kind              = var.storageAccount.kind
+  account_replication_type  = var.storageAccount.account_replication_type
+  account_tier              = var.storageAccount.account_tier
+  min_tls_version           = var.storageAccount.tls_version
 
-  allow_nested_items_to_be_public = false
-  shared_access_key_enabled       = true
+  is_hns_enabled                    = var.storageAccount.HNS_enabled
+  allow_nested_items_to_be_public   = false
+  shared_access_key_enabled         = true
 }
 
 # Create a Storage Queue within the Storage Account
 resource "azurerm_storage_queue" "queue" {
-  name                 = var.storage_account.queueservice.name
+  name                 = var.storageAccount.queueservice.name
   storage_account_name = azurerm_storage_account.sa.name
 }
